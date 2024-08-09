700263b51fca7f194575f4026368f35181284012
死夢めぐり <102138304+ShiyumeMeguri@users.noreply.github.com>
2024-08-10 06:34:52 +0900

    Update PrintModellayer.py

--- Model/PrintModellayer.py
+++ Model/PrintModellayer.py
@@ -13,15 +13,15 @@ def load_model(path: Path, device: str) -> dict[str, torch.Tensor]:
         ckpt = torch.load(path, map_location=device)
         return ckpt.get("state_dict", ckpt)
 
-def print_layer_info(model: dict[str, torch.Tensor], directory: Path):
-    info_path = directory / "model_info.txt"
-    max_name_length = max(len(name) for name in model.keys())  # 计算最长层名称长度
+def print_layer_info(model: dict[str, torch.Tensor], directory: Path, model_filename: str):
+    info_path = directory / f"{model_filename}ModelInfo.txt"
+    max_name_length = max(len(name) for name in model.keys())
     header_format = f"{{:<{max_name_length + 10}}} {{:<20}} {{:>10}} {{:<30}}\n"
     row_format = f"{{:<{max_name_length + 10}}} {{:<20}} {{:10.3f}} {{:<30}}\n"
 
     with open(info_path, 'w') as f:
         f.write(header_format.format('Layer', 'Dtype', 'Size (MB)', 'Shape'))
-        f.write("-" * (max_name_length + 70) + "\n")  # 根据最长名称调整分隔线长度
+        f.write("-" * (max_name_length + 70) + "\n")
         for name, tensor in model.items():
             size_mb = tensor.nelement() * tensor.element_size() / (1024 ** 2)
             f.write(row_format.format(name, str(tensor.dtype), size_mb, str(tensor.size())))
@@ -29,7 +29,8 @@ def print_layer_info(model: dict[str, torch.Tensor], directory: Path):
 def main(input_path: str):
     input_path = Path(input_path)
     input_model = load_model(input_path, "cpu")
-    print_layer_info(input_model, input_path.parent)
+    model_filename = input_path.stem  # 获取文件名（不包含后缀）
+    print_layer_info(input_model, input_path.parent, model_filename)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
